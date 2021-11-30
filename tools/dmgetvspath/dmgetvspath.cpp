f17a494d632636fd497f8b23b880fc0ff5b2ed28
brinkqiang <brink.qiang@gmail.com>
2021-11-30 19:32:34 +0800

    add

--- tools/dmgetvspath/dmgetvspath.cpp
+++ tools/dmgetvspath/dmgetvspath.cpp
@@ -4,27 +4,44 @@
 #include "dmstrtk.hpp"
 #include "dmutil.h"
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
     Iexecute* execute = executeGetModule();
 
     if (NULL != execute)
     {
-        auto strPath = getenv("path");
         std::string strCwd = DMGetWorkPath();
 
-        typedef strtk::std_string::token_deque_type match_list_type;
-        match_list_type match_list;
-        strtk::ifind_all(strCwd, strPath, std::back_inserter(match_list));
+        std::string strCmd = fmt::format(R"(REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\devenv.exe)");
+        std::string strRet = execute->exec(strCmd);
 
-        if (!match_list.empty())
+        std::vector<std::string> vecList;
+        auto it = strRet.find_first_of(R"(")");
+        while (it != std::string::npos)
         {
-            execute->exec("echo OK!");
-            return 0;
+            auto it2 = strRet.find_first_of(R"(")", it + 1);
+
+            if (it2 != std::string::npos)
+            {
+                std::string strPath = strRet.substr(it, it2 - it + 1);
+                auto it3 = strPath.find("Enterprise");
+                if (it3 != std::string::npos)
+                {
+                    strPath = strPath.substr(0, it3);
+                    strPath = strPath + R"(VC\Auxiliary\Build\vcvarsall.bat")";
+                    vecList.push_back(strPath);
+                }
+            }
+
+            it = it2;
         }
 
-        std::string strCmd = fmt::format("setx /m path \"{};{}\"", strPath, strCwd);
-        execute->exec(strCmd);
+        for (auto str : vecList)
+        {
+            std::string strRet = execute->exec(fmt::format("call {} {}", str, "amd64"));
+
+        }
+
     }
 
     return 0;
